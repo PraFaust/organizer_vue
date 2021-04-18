652cb3c20a8e69f1001db4df3416650385ac122e
PraFaust <maximnsuslov@gmail.com>
2021-04-18 15:16:42 +0300

    template change

--- src/main.js
+++ src/main.js
@@ -3,6 +3,20 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+import $ from 'jquery'
+
+global.jQuery = $
+global.$ = $
+
+Vue.config.productionTip = false
+
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+
 Vue.config.productionTip = false
 
 new Vue({
