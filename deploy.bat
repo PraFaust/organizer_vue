6b55dc6dd9d35bb019a55422c005000b01f19615
PraFaust <maximnsuslov@gmail.com>
2021-06-13 12:44:14 +0300

    deploy batch fix

--- deploy.bat
+++ deploy.bat
@@ -1,9 +1,11 @@
 @echo off
 set dist_build=%CD%
-echo @echo off>> %dist_build%deploy.bat
-echo npm run build>> %dist_build%deploy.bat
-call %dist_build%deploy.bat
+echo @echo off>> %dist_build%\deploy-temp.bat
+echo npm run build>> %dist_build%\deploy-temp.bat
+call %dist_build%\deploy-temp.bat
 echo build done, publish...
+erase deploy-temp.bat
 cd dist
 @az webapp up --location westeurope --name clientorganizer --html
 pause
+@echo off
