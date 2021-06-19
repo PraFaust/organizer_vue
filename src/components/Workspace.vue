237356f37e1cc871caee1369e31d9d1e589d63b9
PraFaust <maximnsuslov@gmail.com>
2021-06-19 18:54:43 +0300

    add vue-smooth-dnd, fix package(vue and vue-template version), add DnD behavior

--- src/components/Workspace.vue
+++ src/components/Workspace.vue
@@ -5,18 +5,24 @@
         <b-row class="justify-content-center">
             <b-col lg = 3>
               <b-card title="Widgets">
-                <div v-for="widget in listComponent" :key="widget.name" @click="add_w(widget)">
-                  <component v-bind:is="widget" destination="mini"></component>
-                </div>
+                <l-container @drop="onDrop" group-name="wg">
+                  <l-draggable v-for="widget in listComponent" :key="widget.name">
+                    <div class="draggable-item" @click="add_w(widget)">
+                      <component v-bind:is="widget" destination="mini"></component>
+                    </div>
+                  </l-draggable>
+                </l-container>
               </b-card>
             </b-col>
             <b-col lg = 9>
-                <b-card title="Drop here...">
-                  <transition-group name="slide-fade" mode="out-in" tag="div">
+                <b-card title="Drop here or click on <Widgets>...">
+                  <l-container group-name="wg">
+                   <transition-group name="slide-fade" mode="out-in" tag="div">
                     <div v-for="(widget, i) in workComponent" :key="widget + i">
                         <component v-bind:is="widget" destination="normal"></component>
                     </div>
                   </transition-group>
+                  </l-container>
                 </b-card>
             </b-col>
         </b-row>
@@ -26,6 +32,8 @@
 
 <script>
 const translate = require('../utils/translate.js');
+
+import {Container, Draggable} from "vue-smooth-dnd";
 
 import WCalc from '@/components/widjets/calc.vue'
 import WNotes from '@/components/widjets/notes.vue'
@@ -39,6 +47,8 @@ const included_components = {
   'w-timer': WTimer,
   'w-todo': Wtodo,
   'w-weather': WWeather,
+  'l-container': Container,
+  'l-draggable': Draggable,
 }
 
 
@@ -54,7 +64,9 @@ export default {
   },
   created:function(){
     for(var comp in included_components){
-      this.listComponent.push(comp);
+      if(comp.indexOf('w-') === 0){
+        this.listComponent.push(comp);
+      }
     }
   },
   computed: {
@@ -73,9 +85,27 @@ export default {
     get_text: function(json_name){
       return translate.translate_get_string_js(json_name, this.languge);
     },
-    add_w: function(w){
-      this.workComponent.push(w);
-    }
+    add_w: function(widjet){
+      this.workComponent.push(widjet);
+      console.log(`CLCK on ${widjet}`);
+    },
+    onDrop(dropResult) {
+      console.log(`drop END`);
+      this.applyDrag(this.items, dropResult);
+    },
+    applyDrag(arr, dragResult){
+      const { removedIndex, addedIndex, payload } = dragResult;
+
+      console.log(`removedIndex ${removedIndex}, addedIndex ${addedIndex}`);
+      console.log(this.listComponent[removedIndex]);
+
+      if(removedIndex != null){
+        this.workComponent.push(this.listComponent[removedIndex]);
+      }
+      console.log("arr:");
+      console.dir(arr);
+      //return result;
+    },
   }
 }
 </script>
